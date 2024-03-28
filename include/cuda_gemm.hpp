0820dae62d5616f8f8cf8ddc71dbbb6aeca5e102
luliyucoordinate <coordinate_1995@163.com>
2024-03-28 20:17:34 +0800

    add lly_gemm_v00

--- include/cuda_gemm.hpp
+++ include/cuda_gemm.hpp
@@ -4,6 +4,12 @@
 #include <cuda_runtime.h>
 
 template <typename T>
+void launch_lly_gemm_kernel_v00(size_t m, size_t n, size_t k, T const* alpha,
+                                T const* A, size_t lda, T const* B, size_t ldb,
+                                T const* beta, T* C, size_t ldc,
+                                cudaStream_t stream);
+
+template <typename T>
 void launch_gemm_kernel_v00(size_t m, size_t n, size_t k, T const* alpha,
                             T const* A, size_t lda, T const* B, size_t ldb,
                             T const* beta, T* C, size_t ldc,
--- src/profile_cuda_gemm_fp16.cu
+++ src/profile_cuda_gemm_fp16.cu
@@ -14,9 +14,6 @@ int main()
     __half const fp16_abs_tol{__float2half(5.0e-2f)};
     double const fp16_rel_tol{1.0e-1f};
 
-    __half const fp16_tensor_core_abs_tol{__float2half(5.0e-2f)};
-    double const fp16_tensor_core_rel_tol{1.0e-2f};
-
     constexpr size_t m{4096U};
     constexpr size_t k{4096U};
     constexpr size_t n{4096U};
@@ -46,23 +43,7 @@ int main()
                            size_t, __half const*, size_t, __half const*,
                            __half*, size_t, cudaStream_t)>>> const
         gemm_fp16_kernel_launch_functions{
-            {"Custom GEMM Kernel V00", launch_gemm_kernel_v00<__half>},
-            {"Custom GEMM Kernel V01", launch_gemm_kernel_v01<__half>},
-            {"Custom GEMM Kernel V02", launch_gemm_kernel_v02<__half>},
-            {"Custom GEMM Kernel V02 Vectorized",
-             launch_gemm_kernel_v02_vectorized<__half>},
-            {"Custom GEMM Kernel V03", launch_gemm_kernel_v03<__half>},
-            {"Custom GEMM Kernel V03 Vectorized",
-             launch_gemm_kernel_v03_vectorized<__half>},
-            {"Custom GEMM Kernel V04", launch_gemm_kernel_v04<__half>},
-            {"Custom GEMM Kernel V04 Vectorized",
-             launch_gemm_kernel_v04_vectorized<__half>},
-            {"Custom GEMM Kernel V05", launch_gemm_kernel_v05<__half>},
-            {"Custom GEMM Kernel V05 Vectorized",
-             launch_gemm_kernel_v05_vectorized<__half>},
-            {"Custom GEMM Kernel V06", launch_gemm_kernel_v06<__half>},
-            {"Custom GEMM Kernel V06 Vectorized",
-             launch_gemm_kernel_v06_vectorized<__half>}};
+            {"Custom lly GEMM Kernel V00", launch_lly_gemm_kernel_v00<__half>}};
 
     for (auto const& gemm_fp16_kernel_launch_function :
          gemm_fp16_kernel_launch_functions)
@@ -72,30 +53,6 @@ int main()
             profile_gemm<__half>(
                 m, n, k, lda, ldb, ldc, gemm_fp16_kernel_launch_function.second,
                 fp16_abs_tol, fp16_rel_tol, num_repeats, num_warmups)};
-        std::cout << std::endl;
-    }
-
-    std::vector<std::pair<
-        std::string,
-        std::function<void(size_t, size_t, size_t, __half const*, __half const*,
-                           size_t, __half const*, size_t, __half const*,
-                           __half*, size_t, cudaStream_t)>>> const
-        gemm_fp16_tensor_core_kernel_launch_functions{
-            {"Custom GEMM Kernel V07", launch_gemm_kernel_v07<__half>},
-            {"Custom GEMM Kernel V07 Vectorized",
-             launch_gemm_kernel_v07_vectorized<__half>}};
-
-    for (auto const& gemm_fp16_tensor_core_kernel_launch_function :
-         gemm_fp16_tensor_core_kernel_launch_functions)
-    {
-        std::cout << gemm_fp16_tensor_core_kernel_launch_function.first
-                  << std::endl;
-        std::pair<__half, __half> const gemm_kernel_profile_result{
-            profile_gemm<__half>(
-                m, n, k, lda, ldb, ldc,
-                gemm_fp16_tensor_core_kernel_launch_function.second,
-                fp16_tensor_core_abs_tol, fp16_tensor_core_rel_tol, num_repeats,
-                num_warmups)};
         std::cout << std::endl;
     }
 
